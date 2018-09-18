3b9bc850f317579cf2ee9be62be1a226c44efa04
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 05:55:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,6 @@
 var playlist = { "Slowdive": "Alison", "My Bloody Valentine": "Sometimes" };
 console.log(playlist);
 
-playlist['Slowdive'] = 'Alison'
-      playlist['My Bloody Valentine'] = 'Sometimes'
-
 function updatePlaylist(playlist, artistName, songTitle) {
   var newPlaylist = Object.assign({}, playlist, { [artistName]: songTitle });
   console.log(newPlaylist);
