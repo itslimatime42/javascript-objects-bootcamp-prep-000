110925c2e71fbbf2a4b02f6cd9356fefec11dedf
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 06:01:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ playlist['My Bloody Valentine'] = 'Sometimes'
 console.log(playlist);
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var newPlaylist = Object.assign({}, playlist, { [artistName]: songTitle });
+  var newPlaylist = Object.assign({}, playlist, { 'Phil Ochs': "Here's to the State of Mississippi" });
   console.log(newPlaylist);
   return newPlaylist
 }
