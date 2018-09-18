aca1af507c1f8a39dcaabb22f06c5a841a1b2da2
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 05:51:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   var newPlaylist = Object.assign({}, playlist, { [artistName]: songTitle });
   console.log(newPlaylist);
 }
+updatePlaylist(playlist, beatles, "Norwegian Wood");
 
 function removeFromPlaylist(oldplaylist, artist) {
   delete oldplaylist.artist;
