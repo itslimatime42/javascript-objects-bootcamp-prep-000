388a0b38cb7e5b608a6d7b88453f7d85e406e32d
Robert Schulte <schulte.robertw@outlook.com>
2018-09-18 05:44:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { stones: "Can't You Hear Me Knockin'" };
+var playlist = { stones: "Can't You Hear Me Knockin" };
 
 function updatePlaylist(oldplaylist, artistName, songTitle) {
   var newPlaylist = Object.assign({}, oldplaylist, { [artistName]: songTitle });
